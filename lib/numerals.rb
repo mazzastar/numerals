4970fa7d01369c5b07ae71ba4bdc52949579c45a
Alexander Wong Makers <aw12409@my.bristol.ac.uk>
2014-04-10 06:10:09 +0100

    refactored

--- lib/numerals.rb
+++ lib/numerals.rb
@@ -1,61 +1,44 @@
-
-
-
-def length(number)
-	number.to_s.length
-end
+UNITS = [:ones, :tens, :hundreds, :thousands]
+NUMERALS = ["I", "V", "X", "L", "C", "D", "M", "_", "_"]
 
 def number_to_array(number)
 	number.to_s.chars.map{|char| char.to_i}
 end
 
 def unitCharacters(unit_to_find)
-	units=[:ones, :tens, :hundreds, :thousands, :tenthousands]
-	numerals = ["I", "V", "X", "L", "C", "D", "M"]
-	num_index = units.index(unit_to_find)
+	num_index = UNITS.index(unit_to_find)
 	startIndex = 2*(num_index)
 	endIndex = 2*(num_index+1)
-	numerals[startIndex..endIndex]
+	NUMERALS[startIndex..endIndex]
 end
 
 def units_array(length)
-	units=[:ones, :tens, :hundreds, :thousands, :tenthousands]
-	units[0..(length-1)].reverse
+	UNITS[0..(length-1)].reverse
 end
 
-def format_numeral(number, numeral, numeral_5, numeral_10)
-		if number >= 0 && number < 4
-			numeral*number 
-		elsif number ==4
-			numeral+numeral_5		
-		elsif (number >4 && number <9)
-		 	 numeral_5+numeral*(number-5) 
-		else
-			 numeral+numeral_10
-		end
+def format_numerals(number, num_1, num_5, num_10)
+	case number 
+		when 0..3 then 	num_1*number 
+		when 4 then 	num_1+num_5		
+		when 5..8 then 	num_5+num_1*(number-5)
+		else			num_1+num_10		
+	end
 end
 
 def translate_number(number)
 	number_array = number_to_array(number)
 	char_set = character_sets(number_array.length)
-
 	results = number_array.map.with_index do |number, index|
-		numeral, numeral_5, numeral_10=char_set[index]
-		format_numeral(number, numeral, numeral_5, numeral_10)
+		num_1, num_5, num_10=char_set[index]
+		format_numerals(number, num_1, num_5, num_10)
 	end
 	results.join("")
 end
 
-
-
 def character_sets(length)
 	array_to_return = units_array(length).map do |units|
 		unitCharacters(units)	
-	end
-
-	array_to_return[0]=["M", "_", "_"] if length ==4
-	array_to_return
-	
+	end	
 end
 
 def main()
@@ -68,7 +51,6 @@ def main()
 	end
 
 	puts translate_number(number)
-
 end
 
 main
